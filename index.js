57ef25333b7e60ae4e616ed143b78839e6ef978c
Hvcker19 <110815637+Hvcker19@users.noreply.github.com>
2024-10-12 21:27:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import express from "express";
-import viewEngine from "./config/viewEngine";
-import initWebRoute from "./routes/web";
-import bodyParser from "body-parser";
+const express = require("express");
+const viewEngine = require("./config/viewEngine");
+const initWebRoute = require("./routes/web");
+const bodyParser = require("body-parser");
 
-let app = express();
+const app = express();
 
 // config view engine
 viewEngine(app);
 
-//use body-parser to post data
+// use body-parser to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // init all web routes
 initWebRoute(app);
 
-// Remove process.env and directly set the port
-let port = 8080;
+// Directly set the port
+const port = 8080;
 
 app.listen(port, () => {
     console.log(`App is running at the port ${port}`);
